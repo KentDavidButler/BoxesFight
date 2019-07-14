2949e95d86cfd4e73bc6066e85792c5b7c0b89dc
Kent Butler-HomeDesktop <KentDavidButler@gmail.com>
2019-07-14 13:32:23 -0400

    updated EnemyMovement to turn piror to moving towards player

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,43 +4,51 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-
+    //setup enemy
     Rigidbody m_Rigidbody;
     float m_Speed;
-    //float m_Turn;
-    bool canMove = false;
+    float m_Turn = 5.0f;
+
+    //pull public variable from BBox script within BoundingBox Object
+    GameObject boundingBox;
+    bool canMove;
+
+    //Setup Player info
     Vector3 playerLocation;
     public GameObject player;
-    GameObject boundingBox; 
 
     // Start is called before the first frame update
     void Start()
     {
         //Fetch the Rigidbody component you attach from your GameObject
         m_Rigidbody = GetComponent<Rigidbody>();
-        //Set the speed of the GameObject
         m_Speed = 4.0f;
-        //m_Turn = 100.0f;
 
+        //pulling variable from script connected to boundingBox game object
         boundingBox = GameObject.Find("BoundingBox");
         canMove = boundingBox.GetComponent<BBox>().playerInBox;
-
     }
 
     // Update is called once per frame
     void Update()
     {
         playerLocation = player.transform.position;
-        transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+        //check if player is within box or not
         canMove = boundingBox.GetComponent<BBox>().playerInBox;
 
-        Debug.Log(canMove);
         if (canMove)
         {
+            //rotate enemy towards player
+            var rotation = Quaternion.LookRotation(playerLocation - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * m_Turn);
 
-            transform.LookAt(player.transform);
-            transform.position += transform.forward * m_Speed * Time.deltaTime;
-           
+            //check if player is infront of enemy if so move towards player
+            float angel = Vector3.Angle(transform.forward, playerLocation - transform.position);
+            if (Mathf.Abs(angel) < 60)
+            {
+                transform.position += transform.forward * m_Speed * Time.deltaTime;
+            }
+            
         }
 
     }
